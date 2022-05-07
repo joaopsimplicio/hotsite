4a3eff3694e2c1ca1a31a7530e0e2d70e91c266f
joaopsimplicio <simplicio.joao@escola.pr.gov.br>
2022-05-07 17:10:47 -0300

    Envio do projeto

---
+++ site.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(
+    function () {
+        $("#formCadastro").submit(function (e) {
+            e.preventDefault();
+            Cadastrar();
+        }
+        );
+    }
+);
+
+function Cadastrar() {
+    let parametros = {
+        Nome: $("#nome").val(),
+        Email: $("#email").val(),
+        Mensagem: $("#mensagem").val(),
+    };
+
+    $.post("/Home/Cadastrar", parametros)
+
+    .done(function(data)
+    {
+        if (data.status == "OK") {
+            $("#formCadastro").after("<h3>Cadastro efetuado com sucesso!</h3>");
+            $("#formCadastro").hide();
+        } else {
+            $("#formCadastro").after("<h3>Erro no cadastro...</h3>");
+            $("#formCadastro").hide();
+        }
+    })
+    .fail(function()
+    {
+        alert("Erro no servidor, tente mais tarde");
+    })
+}
